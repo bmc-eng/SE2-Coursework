34a8c9972bc9a7115db8d4d0f3d4ab786b983d7c
LOdweebles <46543579+LOdweebles@users.noreply.github.com>
2023-03-12 16:12:17 +0000

    Added More fields to the customer class along with get and change methods.

--- NewBank/newbank/server/Customer.java
+++ NewBank/newbank/server/Customer.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 public class Customer {
 	
 	private ArrayList<Account> accounts;
+	private String customerEmailAddress;
+	private String customerAddress;
+	private String customerPhoneNumber; 
 	
-	public Customer() {
+	public Customer(String email, String address, String phone) {
+		accounts = new ArrayList<>();
+		customerEmailAddress = email; 
+		customerAddress = address; 
+		customerPhoneNumber = phone; 
+	}
+
+	public Customer(){
 		accounts = new ArrayList<>();
 	}
 	
@@ -20,5 +30,35 @@ public class Customer {
 
 	public void addAccount(Account account) {
 		accounts.add(account);		
+	}
+
+	public String getEmail(){
+		return customerEmailAddress; 
+	}
+
+	public String getAddress(){
+		return customerAddress;
+	}
+
+	public String getPhoneNumber(){
+		return customerPhoneNumber;
+	}
+
+	public void changeEmail(String newEmail){
+		customerEmailAddress = newEmail;
+	}
+
+	public void changeAddress(String newAddress){
+		customerAddress = newAddress; 
+	}
+
+	public void changePhoneNumber(String newPhone){
+		customerPhoneNumber = newPhone; 
+	}
+
+	public void printInfo(){
+		System.out.println("Address: " + customerAddress);
+		System.out.println("Phone Number: " + customerPhoneNumber);
+		System.out.println("Email: " + customerEmailAddress);
 	}
 }
