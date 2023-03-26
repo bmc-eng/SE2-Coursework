30590756d1ddff0a64c55396628fa7d104c4eb9c
Sam Spencer <sam95@vt.edu>
2023-03-26 15:46:09 +0100

    Additional methods to alter balance (credit/debit)

--- NewBank/newbank/server/Account.java
+++ NewBank/newbank/server/Account.java
@@ -5,20 +5,35 @@ import java.io.Serializable;
 public class Account implements Serializable {
 	
 	private String accountName;
-	private double openingBalance;
+	private double Balance;
 	private String type;
 
 	public Account(String accountName, double openingBalance) {
 		this.accountName = accountName;
-		this.openingBalance = openingBalance;
+		this.Balance = openingBalance;
+
 	}
 	
 	public String toString() {
-		return (accountName + ": " + openingBalance);
+		return (accountName + ": " + Balance);
 	}
 
 	public String getType(){
 		return this.type;
+	}
+
+	public Double getBalance(){
+		return this.Balance;
+	}
+
+	// Mutator method to move money out (debit) of the account
+	public void Debit(double debitAmount){
+		this.Balance -= debitAmount;
+	}
+
+	// Mutator method to move money into (credit) of the account
+	public void Credit(double creditAmount){
+		this.Balance += creditAmount;
 	}
 
 }
