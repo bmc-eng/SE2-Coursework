029c657aac439ad57dead01cfba026bf43f298d2
bmc-eng <bclarkeuk@googlemail.com>
2023-04-02 16:53:19 +0100

    Implemented createAccount in new client model

--- NewBank/newbank/client/NewBankClient.java
+++ NewBank/newbank/client/NewBankClient.java
@@ -8,7 +8,6 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.io.Console;
-//import org.json.JSONObject;
 
 
 public class NewBankClient extends Thread{
@@ -27,9 +26,7 @@ public class NewBankClient extends Thread{
 			
 			// ask for password - use Console to mask the password from screen
 			System.out.println("Enter Password");
-			Console console = System.console();
-			char[] passwordArray = console.readPassword();
-			String password = new String (passwordArray);
+			String password = passwordInput();
 
 			// Convert to dictionary - Need to implement JSON method
 			HashMap<String, String> loginDetails = new HashMap<String, String>();
@@ -45,6 +42,52 @@ public class NewBankClient extends Thread{
 		}
 
 	}
+
+	private String passwordInput(){
+		Console console = System.console();
+		char[] passwordArray = console.readPassword();
+		return new String (passwordArray);
+	}
+
+	private String createAccount(BufferedReader userInput){
+
+		try{
+			System.out.println("Username: ");
+			String userName = userInput.readLine();
+
+			// Password input
+			System.out.println("Password: ");
+			String password = passwordInput();
+
+			// Reenter password to check
+			System.out.println("Re-enter password:");
+			String repeatPassword = passwordInput();
+
+			if (!password.equals(repeatPassword)){
+				System.out.println("Passwords do not match. Try again...");
+				return createAccount(userInput);
+			}
+
+			// TO DO: Encrypt password before writing to database
+			System.out.println("First Name: ");
+			String firstName = userInput.readLine();
+			System.out.println("Last Name: ");
+			String lastName = userInput.readLine();
+			System.out.println("email: ");
+			String email = userInput.readLine();
+			System.out.println("address: ");
+			String address = userInput.readLine(); //TODO we should probably make addresses their own class so that they can be printed neatly
+			System.out.println("phone number: ");
+			String phone = userInput.readLine();
+			
+			return userName + "\t" + password + "\t" + firstName + "\t" + lastName + "\t" + email + "\t" + address + "\t" + phone; 
+		}
+		catch(IOException e){
+			System.out.println("Account creation failed; please try again");
+			return createAccount(userInput);
+		}
+
+	}
 	
 	public NewBankClient(String ip, int port) throws UnknownHostException, IOException {
 		
@@ -56,19 +99,9 @@ public class NewBankClient extends Thread{
 		server = new Socket(ip,port); 
 		bankServerOut = new PrintWriter(server.getOutputStream(), true); 
 		BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
-		//bankServerResponceThread.start();
 
 		//Send initial details
 		bankServerOut.println(initialDetails);
-
-		/*
-		try{
-			
-			//bankServerIn.read(initialDetails.toCharArray());
-		} catch (IOException ioe){
-			System.out.println(ioe.toString());
-		}
-		*/
 		
 		bankServerResponceThread = new Thread() {
 			 
@@ -138,7 +171,7 @@ public class NewBankClient extends Thread{
 				return login(userInput);
 			}
 			if (response.contains("2")){
-				//createAccount();
+				return createAccount(userInput);
 			}
 			if (response.contains("3")){
 				// exit the thread
@@ -155,7 +188,6 @@ public class NewBankClient extends Thread{
 	
 	public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
 		// Starting point of the notebook
-
 		new NewBankClient("localhost",14002).start();
 	}
 }
