5d1193c52baf67700002c990ce8596ff709f6b3f
Sam Spencer <sam95@vt.edu>
2023-04-03 19:33:45 +0100

    Beginning work on a Loan object

---
+++ NewBank/newbank/server/Loan.java
@@ -0,0 +1,11 @@
+package newbank.server;
+
+public class Loan {
+    private double rate;
+    private double initialAmount;
+    private double currentBalance;
+    private boolean 
+    public Loan(){
+
+    }
+}
