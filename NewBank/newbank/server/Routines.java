585f1f40ff0f79aad650eda658013c282207c0f7
Sam Spencer <sam95@vt.edu>
2023-04-09 20:49:27 +0100

    Adding regular routines; Method to find all files

--- NewBank/newbank/server/Routines.java
+++ NewBank/newbank/server/Routines.java
@@ -1,30 +1,37 @@
-// package newbank.server;
+package newbank.server;
 
-// import java.util.Set;
-// import java.util.TimerTask;
+import java.io.File;
+import java.util.Set;
+import java.util.TimerTask;
 
-// public class Routines extends TimerTask{
+public class Routines extends TimerTask{
 
-//     // This method returns all files in a given directory
-//     public Set <String> getFiles(){
-//         Set<String> = new Set<E>() {
-            
-//         };
-//     }
+    // This method returns all files in a given directory
+    public Set<String> getFiles(String dir){
+        Set<String> results = new Set<String>();
+        File[] files = new File(dir).listFiles();
+        for (File file : files) {
+            // Select only .obj files
+            if (file.getName().contains(".obj")) {
+                results.add(file.getName());
+            }
+        }
+        return results;
+    }
 
-//     public void updateSavingsInterest(){
-//         // Run through all savings accounts and add interest calculations
+    public void updateSavingsInterest(){
+        // Run through all savings accounts and add interest calculations
 
-//     }
+    }
 
-//     // Create a similar method for calculating all loan balances
+    // Create a similar method for calculating all loan balances
 
-//     // Create a monthly routine to debit accounts
+    // Create a monthly routine to debit accounts
 
-//     // Create a regular routine for direct debits
+    // Create a regular routine for direct debits
 
-//     public void run(){
-//         // Update savings interest
-//         updateSavingsInterest();
-//     }
-// }
+    public void run(){
+        // Update savings interest
+        updateSavingsInterest();
+    }
+}
