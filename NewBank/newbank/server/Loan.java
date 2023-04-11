ff65e479e699c277f33786957a23313bf3a63486
BradSallen <81481332+BradSallen@users.noreply.github.com>
2023-04-11 17:34:44 +0100

    Added additional functions for the loan facility functions include: addloan, updateloan, serialize loan

--- NewBank/newbank/server/Loan.java
+++ NewBank/newbank/server/Loan.java
@@ -1,18 +1,25 @@
 package newbank.server;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
 
-public class Loan {
-    private double rate;
+public class Loan implements Serializable{
+    private ArrayList<String> LoanRecord;
+    private double rate = 0.1;
     private double initialAmount;
     private double currentBalance;
     private int term;
     private int paymentsMade = 0;
     private double interestAccumulated = 0;
+    private String type;
 
-    public Loan(double amount, double rate, int term){
+    public Loan(double amount, int term, String type){
         this.initialAmount = amount;
         this.currentBalance = amount;
-        this.rate = rate;
         this.term = term;
+
+        LoanRecord = new ArrayList<String>();
+        LoanRecord.add(getTimeForLog() + "Loan Accepted For the Ammount: " + initialAmount);
     }
 
     public int paymentsRemaining(){
@@ -25,6 +32,7 @@ public class Loan {
 
     public void makePayment(double amount){
         currentBalance -= amount;
+        paymentsMade = paymentsMade+1;
     }
 
     public void generateInterest(){
@@ -40,4 +48,16 @@ public class Loan {
     public double getInterestAccumulated(){
         return this.interestAccumulated;
     }
+
+    public double getInterestrate(){
+        return this.rate;
+    }
+
+    private String getTimeForLog(){
+		return Instant.now().toString();
+	}
+
+    public String getType(){
+		return this.type;
+	}
 }
